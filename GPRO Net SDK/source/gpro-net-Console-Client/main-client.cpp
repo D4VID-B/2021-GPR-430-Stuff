389737f9cdab596b124a7a461ef984579be065d0
david.bakaleinik <david.bakaleinik@2021GRIDVM-021>
2021-02-04 20:38:01 +0100

    Client-Server basic link done

--- GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
+++ GPRO Net SDK/source/gpro-net-Console-Client/main-client.cpp
@@ -41,107 +41,78 @@ enum GameMessages
 
 int main(int const argc, char const* const argv[])
 {
-	//char str[512];
+	const char SERVER_IP[] = "172.16.2.194";
+	const unsigned short SERVER_PORT = 7777;
 
-	//const unsigned short SERVER_PORT = 7777;
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	RakNet::Packet* packet;
 
-	//RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
-	//RakNet::Packet* packet;
+	RakNet::SocketDescriptor sd;
+	peer->Startup(1, &sd, 1);
 
-
-	//if ((str[0] == 'c') || (str[0] == 'C'))
-	//{
-	//	RakNet::SocketDescriptor sd;
-	//	peer->Startup(1, &sd, 1);
-	//	isServer = false;
-	//}
-
-	//if (isServer)
-	//{
-	//	printf("Starting the server.\n");
-	//	// We need to let the server accept incoming connections from the clients
-	//	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
-	//}
-	//else {
-	//	printf("Enter server IP or hit enter for 127.0.0.1\n");
-	//	gets(str);
-	//	if (str[0] == 0) {
-	//		strcpy(str, "127.0.0.1");
-	//	}
-	//	printf("Starting the client.\n");
-	//	peer->Connect(str, SERVER_PORT, 0, 0);
-
-	//}
-
-	//while (1)
-	//{
-	//	for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
-	//	{
-	//		switch (packet->data[0])
-	//		{
-	//		case ID_REMOTE_DISCONNECTION_NOTIFICATION:
-	//			printf("Another client has disconnected.\n");
-	//			break;
-	//		case ID_REMOTE_CONNECTION_LOST:
-	//			printf("Another client has lost the connection.\n");
-	//			break;
-	//		case ID_REMOTE_NEW_INCOMING_CONNECTION:
-	//			printf("Another client has connected.\n");
-	//			break;
-	//		case ID_CONNECTION_REQUEST_ACCEPTED:
-	//		{
-	//			printf("Our connection request has been accepted.\n");
-
-	//			// Use a BitStream to write a custom user message
-	//			// Bitstreams are easier to use than sending casted structures, and handle endian swapping automatically
-	//			RakNet::BitStream bsOut;
-	//			bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
-	//			bsOut.Write("Hello world");
-	//			peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
-	//		}
-	//		break;
-	//		case ID_NEW_INCOMING_CONNECTION:
-	//			printf("A connection is incoming.\n");
-	//			break;
-	//		case ID_NO_FREE_INCOMING_CONNECTIONS:
-	//			printf("The server is full.\n");
-	//			break;
-	//		case ID_DISCONNECTION_NOTIFICATION:
-	//			if (isServer) {
-	//				printf("A client has disconnected.\n");
-	//			}
-	//			else {
-	//				printf("We have been disconnected.\n");
-	//			}
-	//			break;
-	//		case ID_CONNECTION_LOST:
-	//			if (isServer) {
-	//				printf("A client lost the connection.\n");
-	//			}
-	//			else {
-	//				printf("Connection lost.\n");
-	//			}
-	//			break;
-
-	//		case ID_GAME_MESSAGE_1:
-	//		{
-	//			RakNet::RakString rs;
-	//			RakNet::BitStream bsIn(packet->data, packet->length, false);
-	//			bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
-	//			bsIn.Read(rs);
-	//			printf("%s\n", rs.C_String());
-	//		}
-	//		break;
-
-	//		default:
-	//			printf("Message with identifier %i has arrived.\n", packet->data[0]);
-	//			break;
-	//		}
-	//	}
-	//}
+	peer->Connect(SERVER_IP, SERVER_PORT, 0, 0);
+	printf("Starting the client.\n");
 
 
-	//RakNet::RakPeerInterface::DestroyInstance(peer);
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+			{
+				printf("Our connection request has been accepted.\n");
+
+				// Use a BitStream to write a custom user message
+				// Bitstreams are easier to use than sending casted structures, and handle endian swapping automatically
+				RakNet::BitStream bsOut;
+				bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
+				bsOut.Write("Hello world");
+				peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
+			}
+			break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+					printf("We have been disconnected.\n");
+				break;
+			case ID_CONNECTION_LOST:
+					printf("Connection lost.\n");
+				break;
+
+			case ID_GAME_MESSAGE_1:
+			{
+				RakNet::RakString rs;
+				RakNet::BitStream bsIn(packet->data, packet->length, false);
+				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+				bsIn.Read(rs);
+				printf("%s\n", rs.C_String());
+			}
+			break;
+
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
+
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
 
 	//return 0;
 
